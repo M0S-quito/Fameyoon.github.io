2e82cd258a65dcf07aa58c076f85f7b8d16a5252
심영우 <zoojark040815@gmail.com>
2025-07-12 05:54:16 +0000

    vite.config.js에서 fameyoon-incoo.run.goorm.io주소가 접속 가능하게 수정

--- client/twoMB/vite.config.js
+++ client/twoMB/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { // fameyoon-incoo.run.goorm.io에서의 접속도 허용
+    allowedHosts: ['fameyoon-incoo.run.goorm.io']
+  }
 })
