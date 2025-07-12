86246050cf47fb4f55887811ab392acc4c66c0b9
심영우 <zoojark040815@gmail.com>
2025-07-12 06:45:48 +0000

    설정: GitHub Pages 배포를 위한 Vite 설정 및 스크립트 추가

--- client/twoMB/vite.config.js
+++ client/twoMB/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 루트 도메인이므로 슬래시만
   plugins: [react()],
   server: { // fameyoon-incoo.run.goorm.io에서의 접속도 허용
     allowedHosts: ['fameyoon-incoo.run.goorm.io']
